b798ef89db532ebc6ba6777309b48eca89a1db90
Евгений Войтенко <voytenkoei@psbank.ru>
2022-02-09 09:49:08 +0300

    some fix

--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -7,9 +7,17 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./parent.component.css'],
 })
 export class ParentComponent implements OnInit {
-  form = new FormGroup({ parent: new FormControl('', Validators.required) });
+  form = new FormGroup({
+    parent: new FormControl('', Validators.required),
+    child: new FormGroup({
+      adress: new FormControl('', Validators.required),
+      index: new FormControl('', Validators.required),
+    }),
+  });
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.form.valueChanges.subscribe((data) => console.log('data', data));
+  }
 }
