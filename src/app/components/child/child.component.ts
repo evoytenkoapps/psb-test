0fab6176f3a1bcd180a5dbaa04cf6df1e6306e22
Евгений Войтенко <voytenkoei@psbank.ru>
2022-02-09 10:38:14 +0300

    some fix

--- src/app/components/child/child.component.ts
+++ src/app/components/child/child.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, forwardRef, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  FormControl,
+  FormGroup,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
-  styleUrls: ['./child.component.css']
+  styleUrls: ['./child.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => ChildComponent),
+      multi: true,
+    },
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => ChildComponent),
+      multi: true,
+    },
+  ],
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, ControlValueAccessor {
+  public childForm = new FormGroup({
+    adressChild: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(4),
+    ]),
+    indexChild: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  public onTouched: () => void = () => {};
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  registerOnChange(fn: any): void {
+    this.childForm.valueChanges.subscribe(fn);
   }
 
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {}
+
+  writeValue(obj: any): void {
+    console.log('writeValue', obj);
+  }
+
+  validate(c: AbstractControl): ValidationErrors | null {
+    console.log('Basic Info validation', c);
+    return this.childForm.valid
+      ? null
+      : {
+          invalidForm: {
+            valid: false,
+            message: 'basicInfoForm fields are invalid',
+          },
+        };
+  }
 }
--- src/app/components/parent/parent.component.ts
+++ src/app/components/parent/parent.component.ts
@@ -9,15 +9,15 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class ParentComponent implements OnInit {
   form = new FormGroup({
     parent: new FormControl('', Validators.required),
-    child: new FormGroup({
-      adress: new FormControl('', Validators.required),
-      index: new FormControl('', Validators.required),
-    }),
+    child: new FormControl(''),
   });
 
   constructor() {}
 
   ngOnInit(): void {
     this.form.valueChanges.subscribe((data) => console.log('data', data));
+    this.form.statusChanges.subscribe((status) =>
+      console.log('status', status)
+    );
   }
 }
